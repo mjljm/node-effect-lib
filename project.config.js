a0346b01ce116d7d2be21ddb65fbb06ba4905ce0
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-06-21 16:16:12 +0200

    Use deepmerge to merge package.json's

--- project.config.js
+++ project.config.js
@@ -1,33 +1,32 @@
 import * as ConfigManager from '@mjljm/configs-manager';
+import merge from 'deepmerge';
 
 const packageName = 'node-effect-lib';
-const packageJson = {
-	...ConfigManager.PackageBase,
-	...ConfigManager.PackageSubRepo,
-	...ConfigManager.PackageEffectTags,
-	...ConfigManager.PackageSubRepoTranspiled,
-	...ConfigManager.PackageSubRepoPublic,
-	name: ConfigManager.PackageBase.name(packageName),
-	version: '1.0.0',
-	repository: ConfigManager.PackageBase.repository(packageName),
-	bugs: ConfigManager.PackageBase.bugs(packageName),
-	homepage: ConfigManager.PackageBase.homepage(packageName),
-	description: 'A complement to the official @effect/platform library with add-ons for Node.js',
-	scripts: {
-		...ConfigManager.PackageSubRepo.scripts,
-		...ConfigManager.PackageBase.scripts,
-		...ConfigManager.PackageSubRepoTranspiled.scripts,
-		...ConfigManager.PackageSubRepoPublic.scripts
-	},
-	peerDependencies: {
-		'@mjljm-dev/effect-lib': 'workspace:*',
-		'@mjljm-dev/js-lib': 'workspace:*',
-		'@effect/platform': ConfigManager.Versions.effectPlatformVersion,
-		'@effect/platform-node': ConfigManager.Versions.effectPlatformNodeVersion,
-		'@effect/schema': ConfigManager.Versions.schemaVersion,
-		effect: ConfigManager.Versions.effectVersion
+
+const packageJson = merge.all([
+	ConfigManager.PackageBase,
+	ConfigManager.PackageSubRepo,
+	ConfigManager.PackageEffectTags,
+	ConfigManager.PackageSubRepoTranspiled,
+	ConfigManager.PackageSubRepoPublic,
+	{
+		name: ConfigManager.PackageBase.name(packageName),
+		version: '1.0.0',
+		repository: ConfigManager.PackageBase.repository(packageName),
+		bugs: ConfigManager.PackageBase.bugs(packageName),
+		homepage: ConfigManager.PackageBase.homepage(packageName),
+		description: 'A complement to the official @effect/platform library with add-ons for Node.js',
+		peerDependencies: {
+			'@mjljm-dev/effect-lib': 'workspace:*',
+			'@mjljm-dev/js-lib': 'workspace:*',
+			'@effect/platform': ConfigManager.Versions.effectPlatformVersion,
+			'@effect/platform-node': ConfigManager.Versions.effectPlatformNodeVersion,
+			'@effect/schema': ConfigManager.Versions.schemaVersion,
+			effect: ConfigManager.Versions.effectVersion
+		}
 	}
-};
+]);
+
 export default {
 	// Put prettier in first position so the next generated files will get formatted
 	'prettier.config.js': ConfigManager.PrettierConfigTemplate,
